422541dabfbbb2ececed8663d66a3feac395e99d
Rudolf Harmse <rudolf.harmse@gmail.com>
2018-03-20 13:43:55 +0200

    Added more detail to JSON sample config, pooling comments and started rewriting section

--- pooling/pool.go
+++ pooling/pool.go
@@ -6,23 +6,42 @@ import (
 	"time"
 )
 
-type PoolNode struct {
-	hostname string
-	ip       net.IP
-	port     uint16
+//Track the request, response and relevant http response codes.
+type NodeDataTracker struct {
+	reqCnt     uint64 //total req cnt
+	respCnt    uint64 //total resp cnt
+	failCnt    uint64 //total 4xx and 5xx responses
+	http1xxCnt uint64 //convert to map maybe
+	http2xxCnt uint64
+	http3xxCnt uint64
+	http4xxCnt uint64
+	http5xxCnt uint64
 }
 
+//This represents a HTTP Serving node part of a Resource Pool
+type PoolNode struct {
+	hostname   string
+	ip         net.IP
+	port       uint16
+	relCtxRoot string
+}
+
+//Represents a container of HTTP Servers serving client requests
 type Pool struct {
 	name       string
-	members    map[string]*PoolNofde
+	members    map[string]*PoolNode
 	httpClient *http.Client
 }
 
 func (pool *Pool) AddPoolNode(hostname string) error {
-
+	return nil
 }
 
 func (pool *Pool) RemovePoolNode(hostname string) error {
+	return nil
+}
+
+func (pool *PoolNode) BuildNodeURI() error {
 	return nil
 }
 
