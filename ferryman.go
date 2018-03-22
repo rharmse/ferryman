0e5e6d1ca04bafeb53218ef54936c169a0d5aef3
Rudolf Harmse <rudolf.harmse@vodacom.co.za>
2018-03-22 09:39:47 +0200

    Reworked

--- ferryman.go
+++ ferryman.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+    "fmt"
+    "github.com/rharmse/ferryman/lib"
+)
+
 func main() {
 
     conf, error := ferryman.LoadConf()
+    if error == nil {
+        fmt.Println("%v", conf)
+    } else {
+        panic(error)
+    }
+    
 	// 1. Determine if there is configuration, and load from it
 	// 2. Load Rules
 	// 3. Setup Content Rewrites
