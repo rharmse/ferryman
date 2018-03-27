eb160c76072072a1b34d2f447aeabad5ea7d4606
Rudolf <rudolf.harmse@gmail.com>
2018-03-27 15:54:47 +0200

    Added refs to datastruct whitepapers to use for the Router

--- lib/router.go
+++ lib/router.go
@@ -1,1 +1,4 @@
 package ferryman
+
+//Router to implement adaptive radix tree https://pdfs.semanticscholar.org/6abf/5107efc723c655956f027b4a67565b048799.pdf
+//Reference http://daslab.seas.harvard.edu/classes/cs265/files/presentations/CS265_presentation_Sinyagin.pdf
